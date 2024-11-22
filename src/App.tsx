19e811075a8dc7228bdf31153dbcea2a4b7a362b
daviwerneck <davijwerneck@gmail.com>
2024-11-22 08:41:44 -0300

    iniciando...

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Header} from "./components/Header.tsx"
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <h1>Olá, Mundo!</h1> 
+    </>
+  )
+}
+
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
